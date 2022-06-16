88a67faf0e221cb2dc56dcabdb5c1ac070975899
Adil Shaikh <adilshaikhhh@outlook.com>
2022-06-16 21:38:02 +0530

    product module

---
+++ productspringboot/src/main/java/com/tnsif/demo/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.tnsif.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Integer>
+{
+
+}
---
+++ productspringboot/src/main/java/com/tnsif/demo/ProductspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductspringbootApplication.class, args);
+	}
+
+}
